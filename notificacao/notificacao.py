23ecc86996992cb5fbde8492fd17cf2c21d656a1
LuDesigner <luis.designer.oficial@gmail.com>
2025-09-07 13:19:47 -0300

    Atualização do pedido projeto

--- notificacao/notificacao.py
+++ notificacao/notificacao.py
@@ -1,13 +1,6 @@
 from abc import ABC, abstractmethod
 
 class Notificacao(ABC):
+    @abstractmethod
     def enviar_notificacao(self, cliente, mensagem):
         pass
-
-    @abstractmethod
-    def enviar_email(self, cliente, mensagem):
-        print("Enviar email")
-
-    @abstractmethod
-    def enviar_sms(self, cliente, mensagem):
-        print("Enviar sms")
--- pedido/pedido.py
+++ pedido/pedido.py
@@ -5,6 +5,7 @@ class Pedido(ABC):
     def __init__(self, cliente, itens):
         self.cliente = cliente
         self.itens = itens
+        self.status = "Criado!"
 
     @abstractmethod
     def calcular_total(self):
